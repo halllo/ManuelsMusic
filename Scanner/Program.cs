45ef01b7e6f835a18d29cdca811f4f743b16550b
Manuel Naujoks <mnaujoks@gmail.com>
2016-12-18 21:36:27 +0100

    album list

--- Scanner/Program.cs
+++ Scanner/Program.cs
@@ -11,63 +11,98 @@ namespace Scanner
 		[STAThread]
 		public static void Main()
 		{
-			//Console.WriteLine("// Scanning...");
-			//var songs = Scan(directory: @"C:\Users\manue\OneDrive\Musik");
+			/*{//Exporting
+				Console.WriteLine("// Scanning...");
+				const string musicFolder = @"C:\Users\manue\OneDrive\Musik\";
+				var songs = Scan(directory: musicFolder);
 
 
-			//Console.WriteLine("// Exporting...");
-			//var xdoc = new XDocument(
-			//	new XElement("songs",
-			//		songs.Select(song => new XElement("song",
-			//			song.Artist != null ? new XAttribute("artist", song.Artist) : null,
-			//			song.Title != null ? new XAttribute("title", song.Title) : null,
-			//			new XText(song.File)
-			//		))
-			//	)
-			//);
-			//xdoc.Save("songs.xml");
+				Console.WriteLine("// Exporting...");
+				var xdoc = new XDocument(
+					new XElement("songs",
+						songs.Select(song => new XElement("song",
+							song.Artist != null ? new XAttribute("artist", song.Artist) : null,
+							song.Album != null ? new XAttribute("album", song.Album) : null,
+							song.Title != null ? new XAttribute("title", song.Title) : null,
+							new XText(song.File)
+						))
+					)
+				);
+				xdoc.Save("songs.xml");
+			}*/
 
 
-			Console.WriteLine("// Importing...");
-			var songs = XDocument.Load("songs.xml")
-				.Descendants("song")
-				.Select(d => new Song
+			{//Importing
+				Console.WriteLine("// Importing...");
+				var songs = XDocument.Load("songs.xml")
+					.Descendants("song")
+					.Select(d => new Song
+					{
+						Artist = d.Attribute("artist")?.Value,
+						Album = d.Attribute("album")?.Value,
+						Title = d.Attribute("title")?.Value,
+						File = d.Value
+					}).ToList();
+
+
+				//foreach (var song in songs)
+				//{
+				//	Console.WriteLine($"{song.Artist} - {song.Title}");
+				//}
+				//Console.WriteLine($"{songs.Count} songs");
+				//Console.WriteLine($"{songs.GroupBy(s => s.Album).Count()} albums");
+				//Console.WriteLine($"{songs.GroupBy(s => s.Artist).Count()} artists");
+
+
+				var albums = songs.GroupBy(s => new { s.Artist, s.Album });
+				foreach (var album in albums)
 				{
-					Artist = d.Attribute("artist")?.Value,
-					Title = d.Attribute("title")?.Value,
-					File = d.Value
-				}).ToList();
+					Console.ForegroundColor = ConsoleColor.Cyan;
+					Console.WriteLine($"{album.Key.Artist} - {album.Key.Album}");
+					Console.ResetColor();
+					foreach (var song in album)
+					{
+						Console.Write($"- {song.Title}");
+						Console.ForegroundColor = ConsoleColor.DarkGray;
+						Console.WriteLine($" ({string.Concat(song.File.Reverse().Take(80).Reverse())})");
+						Console.ResetColor();
+					}
+					Console.WriteLine();
+				}
+				Console.ReadLine();
+			}
+		}
 
 
-			//foreach (var song in songs)
-			//{
-			//	Console.WriteLine($"{song.Artist} - {song.Title}");
-			//}
-			//Console.WriteLine($"{songs.Count} songs");
-
-
+		//RepairPlaylist("playlist.wpl", musicFolder, songs);
+		static void RepairPlaylist(string playlist, string basePath, List<Song> songs)
+		{
+			Console.WriteLine(playlist);
 			int index = 1;
-			foreach (var playlistItem in XDocument.Load("bestof2012.wpl").Descendants("media"))
+
+			var xDocument = XDocument.Load(playlist);
+			foreach (var playlistItem in xDocument.Descendants("media"))
 			{
 				var artist = playlistItem.Attribute("trackArtist")?.Value;
 				var title = playlistItem.Attribute("trackTitle")?.Value;
 				var file = playlistItem.Value;
 
 				var source = playlistItem.Attribute("src")?.Value;
-				if (!File.Exists(source))
+				if (!File.Exists(basePath + source))
 				{
-					var song = songs.Where(s => s.Artist == artist && s.Title == title).SingleOrDefault();
+					var song = songs.Where(s => s.Artist == artist && s.Title == title).ToList();
 
-					if (song != null)
+					if (song.Count == 1)
 					{
 						Console.ForegroundColor = ConsoleColor.Yellow;
-						Console.WriteLine($"{index.ToString().PadLeft(2)}:\t{string.Concat(playlistItem.ToString().Take(40))} -> {string.Concat(song.File.Reverse().Take(60).Reverse())}");
+						Console.WriteLine($"{index.ToString().PadLeft(2)}:\t{string.Concat(playlistItem.ToString().Take(40))} -> {string.Concat(song[0].File.Reverse().Take(60).Reverse())}");
 						Console.ResetColor();
+						playlistItem.Attribute("src").Value = song[0].File.Replace(basePath, "");
 					}
 					else
 					{
 						Console.ForegroundColor = ConsoleColor.Red;
-						Console.WriteLine($"{index.ToString().PadLeft(2)}:\t{string.Concat(playlistItem.ToString().Take(40))} -> NO_FILE: {artist} - {title}");
+						Console.WriteLine($"{index.ToString().PadLeft(2)}:\t{string.Concat(playlistItem.ToString().Take(40))} -> ? ({artist} - {title})");
 						Console.ResetColor();
 					}
 				}
@@ -76,14 +111,16 @@ namespace Scanner
 					Console.ForegroundColor = ConsoleColor.Green;
 					Console.WriteLine($"{index.ToString().PadLeft(2)}:\t{string.Concat(playlistItem.ToString().Take(100))}");
 					Console.ResetColor();
+					playlistItem.Attribute("src").Value = playlistItem.Attribute("src").Value.Replace(basePath, "");
 				}
 
 				index++;
 			}
-			Console.ReadLine();
+			xDocument.Save(playlist);
 		}
 
-		public static List<Song> Scan(string directory)
+
+		static List<Song> Scan(string directory)
 		{
 			var songs = new List<Song>();
 
@@ -96,6 +133,7 @@ namespace Scanner
 					{
 						File = file,
 						Artist = tag?.Artists.Value,
+						Album = tag?.Album.Value,
 						Title = tag?.Title.Value
 					});
 					Console.Write(".");
@@ -109,6 +147,7 @@ namespace Scanner
 	{
 		public string File { get; set; }
 		public string Artist { get; set; }
+		public string Album { get; set; }
 		public string Title { get; set; }
 	}
 }
